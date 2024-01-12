69821c2c3fc70b7aa8d3fc9a99a69c540a54a75e
Nikhil Bhandari <nikhilbhandari28@gmail.com>
2024-01-12 15:59:40 +0530

    Imported Packages and Defined our target

---
+++ scanner.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import sys
+import socket
+from datetime import datetime
+
+#Define our target
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+	print("Invalid amount of arguements.")
+	print("Syntax: python3 scanner.py <IP Address>")
